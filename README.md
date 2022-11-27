dd2a444e21e1b9d215db3fa709feb6e57ab7552f
MinnAK686 <anastasioak327@gmail.com>
2022-11-27 21:50:06 +0630

    Finished Tuto

---
+++ README.md
@@ -0,0 +1,11 @@
+# Firebase-9-Tutorial-with Netninja
+
+## Project setup
+```
+npm install
+```
+
+### Run index.js
+```
+npm run build
+```
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import { initializeApp } from "firebase/app"
+// Firestore
 import {
-    getFirestore,collection,getDocs,
-    addDoc,doc,deleteDoc, onSnapshot
-} from "firebase/firestore"
+    getFirestore,collection,getDocs,getDoc,
+    addDoc,doc,deleteDoc, onSnapshot,
+    query,where,        // Query
+    orderBy,            // OrderBy
+    serverTimestamp,    // Timestamp 
+    updateDoc,          // Update
 
+} from "firebase/firestore"
+// Auth
+import {
+    getAuth,createUserWithEmailAndPassword,
+    signOut,signInWithEmailAndPassword,
+    onAuthStateChanged
+} from "firebase/auth"
+
+// Firebase Configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDKXa-dsw4hSxjxbJbAmIRyXyvnaGY8LNM",
     authDomain: "fir-9-tuto-c22b6.firebaseapp.com",
@@ -18,8 +31,16 @@ initializeApp(firebaseConfig);
 
 // Init service
 const db = getFirestore();
+const auth = getAuth();
 
+// Collection Reference
 const colRef = collection(db,"books");
+
+// Query
+const q = query(colRef,where("author","==", "admin"))
+
+// OrderBy Query 
+const orderBooks = query(colRef, orderBy("createdAt","desc"))
 
 // Get Collection Data
 // getDocs(colRef)
@@ -32,7 +53,7 @@ const colRef = collection(db,"books");
 //     })
 
 // Get Real Time Data
-onSnapshot(colRef, (snapshot) => {
+onSnapshot(orderBooks, (snapshot) => {
     let books = [];
     snapshot.forEach(doc => {
         books.push({...doc.data(), id: doc.id})
@@ -43,15 +64,14 @@ onSnapshot(colRef, (snapshot) => {
 
 
 // Add Data to Collection
-
 const addData = document.querySelector(".addData");
-
 addData.addEventListener('submit', (e) => {
     e.preventDefault();
 
     addDoc(colRef, {
         title: addData.title.value,
-        author: addData.author.value
+        author: addData.author.value,
+        createdAt: serverTimestamp()
     })
     .then(() => {
         addData.reset()
@@ -60,9 +80,7 @@ addData.addEventListener('submit', (e) => {
 
 
 // Delete Data from Collection
-
 const deleteData = document.querySelector(".deleteData");
-
 deleteData.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -75,3 +93,80 @@ deleteData.addEventListener("submit", (e) => {
 
 })
 
+
+// Get Single Document by ID
+// const docRef = doc(colRef,"EMVM6af1zeTx2FI4u33j");
+
+// onSnapshot(docRef, (doc) => {
+//     console.log(doc.data(), doc.id)
+// })
+
+
+// Update Doc on Realtime
+const updateData = document.querySelector(".updateData");
+const bookID = document.querySelector(".bookID");
+
+updateData.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const docRef = doc(colRef, bookID.value);
+
+    updateDoc(docRef, {
+        title: "Title Updated"
+    }).then((doc) => {
+        console.log(doc.data(), doc.id)
+    })
+
+})
+
+
+// Signup Form
+const signupForm = document.querySelector(".signupForm");
+signupForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let usrEmail = signupForm.email.value
+    let usePassword = signupForm.password.value
+
+    createUserWithEmailAndPassword(auth,usrEmail,usePassword)
+        .then((cred) => {
+            console.log(cred.user);
+            signupForm.reset()
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+})
+
+// Signin 
+const signinForm = document.querySelector(".signinForm");
+signinForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let usrEmail = signinForm.email.value;
+    let usePassword = signinForm.password.value;
+    signInWithEmailAndPassword(auth, usrEmail, usePassword)
+        .then((cred) => {
+            console.log(cred.user);
+            console.log("Signinned")
+            signinForm.reset();
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+})
+
+// Signout
+let signout = document.querySelector(".signout");
+signout.addEventListener("click", () => {
+    signOut(auth).then(() => {
+        console.log("Signout Success");
+    }).catch((err) => {
+        console.log(err.message)
+    })
+})
+
+
+// Auth State
+onAuthStateChanged(auth, (user) => {
+    console.log("User State:\n",user)
+})
